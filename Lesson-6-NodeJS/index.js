08371559c93192c9311ae6ffb769e7db8c8d3929
SohelShahid117 <sohelshahid09@gmail.com>
2024-06-19 23:14:57 +0600

    external modules--npm done

---
+++ Lesson-6-NodeJS/index.js
@@ -0,0 +1,7 @@
+//external modules 
+// console.log("hello world")
+const getRandomFruitsName = require('random-fruits-name')
+console.log(getRandomFruitsName())
+
+var moviesNames = require('movies-names');
+console.log(moviesNames.random()) 
