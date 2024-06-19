53d69eba5b7101bf18eea2907c72f36793ea51a0
SohelShahid117 <sohelshahid09@gmail.com>
2024-06-19 13:22:24 +0600

    fs module done

---
+++ Lesson-2-NodeJS/index.js
@@ -0,0 +1,90 @@
+// node js bangla tutorial 4 : Module | fs module - managing files
+console.log("hi")
+const fs = require("fs")
+// console.log(fs)
+fs.writeFile("demoWriteFile.txt","this is some text.I am a expert programmer",function(err){
+    if(err){
+        console.log(err)
+    }else{
+        console.log("success")
+    }
+})
+// fs.writeFile("demo.txt","A am Sohel.I am an expert programmer",function(err){
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("success")
+//     }
+// })
+
+// fs.writeFile("demo.txt","I love programming.",function(err){
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("success")
+//     }
+// })
+
+
+// fs.appendFile("demo.txt","eNjoy coding life.C,C++,JS,Python.",(err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("success")
+//     }
+// })
+
+
+// fs.readFile("demo.txt",(data,err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("successful")
+//     }
+// })
+
+
+// fs.readFile("demo.txt",(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("successful")
+//         console.log(data)
+//     }
+// })
+
+
+// fs.readFile("demo.txt","utf-8",(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("successful")
+//         console.log(data)
+//     }
+// })
+
+
+// fs.rename("demo.txt","demo2.txt",(err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("successful")
+//     }
+// })
+
+// fs.unlink("demo2.txt",(err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("successful")
+//     }
+// })
+
+
+fs.exists("demo2.txt",(result)=>{
+    if(result){
+        console.log("found")
+    }else{
+        console.log("not found")
+    }
+})
---
+++ Lesson-2-NodeJS/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+fs.rename("demo.txt","demo2.txt",(err)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("successful")
+//     }
+// })
