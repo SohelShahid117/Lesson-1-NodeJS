1edf8effc9e505cb3735be2106191c512a5ce4d8
SohelShahid117 <sohelshahid09@gmail.com>
2024-06-19 12:34:03 +0600

    Local module - exports, require done

---
+++ Lesson-1-NodeJS/student.js
@@ -0,0 +1,18 @@
+const getName = () =>{
+    return "I am Sohel"
+}
+
+const getAge = () =>{
+    return "my age is 34"
+}
+
+const cgpa = 2.83
+// exports.getName = getName
+// exports.getAge = getAge
+// exports.result = cgpa
+
+module.exports = {
+    getName,
+    getAge,
+    cgpa
+}
